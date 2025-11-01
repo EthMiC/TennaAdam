ad1785388a3c1a41c34860eac939e8dbddbb2e86
EthiopianMiddleChild <amannegesse@gmail.com>
2025-11-01 21:22:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var slides_container = document.getElementById("slide");
+var previous_slide_index = 0;
+var current_slide_index = 0;
+
+var slide_interval = setInterval(slide_hero, 5000)
+
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == "visible") {
+        slide_interval = setInterval(slide_hero, 5000)
+    }
+    else {
+        clearInterval(slide_interval)
+    }
+})
+
+function slide_hero() {
+    slides_container.children[previous_slide_index].setAttribute( 'class', '');
+    slides_container.children[current_slide_index].setAttribute( 'class', 'not-active');
+    previous_slide_index = current_slide_index;
+    current_slide_index += current_slide_index < 4 ? 1 : -4;
+    console.log(current_slide_index)
+    slides_container.children[current_slide_index].setAttribute( 'class', 'active');
+}
